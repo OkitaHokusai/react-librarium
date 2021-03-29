b549858044a1c1d2a13f3b52723421d98ea1d0cd
Okita <21800014@aluno.cotemig.com.br>
2021-03-29 11:26:58 -0300

    Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
+import '../../scss/home.scss';
 
+import Blob from '../../assets/path3.png';
 
 const Home = () =>{ 
     return(
-        <>
-         <p>kekw</p>
-        </>
+        <div className='land-page'>
+            <img src={Blob} className='land-page-bg1'/>
+            <img src={Blob} className='land-page-bg1'/>
+            <div className='land-page-title'>
+                <h1>Librarium</h1>
+                <p>O lugar onde histórias mágicas são contadas e sonhos são criados</p>
+                <div className='land-page-links'>
+                    <a className="login-btn" href="/Login">Login</a>
+                    <a className="cad-btn" href="/Cadastrar">Cadastrar</a>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home;
